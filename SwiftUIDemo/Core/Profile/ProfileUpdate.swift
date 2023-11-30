c9433f244dd501bdf8b74c6dc47bf82f7134b809
Nikhil Mallik <121235979+nikhil-mallik@users.noreply.github.com>
2023-11-30 16:22:22 +0530

    Added LaunchScreen, SwiftData Demo and alert to Spinner

---
+++ SwiftUIDemo/Core/Profile/ProfileUpdate.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileUpdate.swift
+//  SwiftUIDemo
+//
+//  Created by Nikhil Mallik on 28/11/23.
+//
+
+import SwiftUI
+
+struct ProfileUpdate: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileUpdate()
+}
